45e114838b325f2f8080f0255f061ca96950563b
Hanifa Chep <chep.hanifa@gmail.com>
2021-06-18 11:27:47 -0700

    Combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible combinations of two digits.
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+int v, w;
+
+for (v = o; v <9; v++)
+{
+for (w = v + 1; w < 10; w++)
+{
+putchar((v % 10) + '0');
+putchar((w % 10) + '0');
+
+if (v == 8 && w == 9)
+continue;
+
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
