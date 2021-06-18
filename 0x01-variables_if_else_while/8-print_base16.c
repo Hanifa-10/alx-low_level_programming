6138b48a56391ad8e6f323f1c58a83e7b8528929
Hanifa Chep <chep.hanifa@gmail.com>
2021-06-18 07:45:48 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - print all numbers of base 16 starting 0
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int hexadecimal = 0;
+
+while (hexadecimal <= 9)
+putchar(hexadecimal++ + '0');
+
+hexadecimal = 'a';
+
+while (hexadecimal <= 'f')
+putchar(hexadecimal++);
+putchar('\n');
+
+return (0);
+}
