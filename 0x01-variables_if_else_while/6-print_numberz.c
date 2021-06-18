1bb0c6f89cc1dcf0f5f5408347b6e870c4b23be2
Hanifa Chep <chep.hanifa@gmail.com>
2021-06-18 07:24:24 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - print all digits of base 10 starting 0
+* without using char
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int base = 0;
+
+while (base <= 9)
+{
+putchar (base + '0');
+base++;
+}
+putchar ('\n');
+
+return (0);
+}
