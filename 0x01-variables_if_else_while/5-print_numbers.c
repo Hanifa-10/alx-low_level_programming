14c4ef1dca08e1806b195c87e1b2c1bbf0b9849f
Hanifa Chep <chep.hanifa@gmail.com>
2021-06-18 06:02:11 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <kstdio.h>
+
+/**
+* main - print all digits of base 10 starting 0
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char base = '0', nine ='9';
+
+while (base <= nine)
+putchar(base++);
+
+putchar('\n');
+
+return (0);
+}
