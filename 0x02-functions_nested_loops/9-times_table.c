551fc9e834886ca2de77cff4a1b2e58da68fa5fd
Hanifa Chep <chep.hanifa@gmail.com>
2021-06-22 10:45:00 -0700

    9-times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "holberton"
+
+/**
+* times_table -prints the 9 times table, starting 0
+*/
+
+void times_table(void)
+{
+int row, column; /* Indexes */
+int limit = 10; /* Limit value */
+int result;
+
+for (row = 0; row < limit; row++)
+{
+for (column = o; column < limit; column++)
+{
+result = row * column;
+
+if (result <= 9)
+{
+if (column > 0)
+_putchar(' ');
+
+_putchar(result + '0');
+}
+else if (result > 9)
+{
+_putchar((result / 10) +'0');
+_putchar((result % 10) + '0');
+}
+if (column < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
+
