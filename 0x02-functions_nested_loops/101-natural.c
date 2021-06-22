7bb429c0379615cdae648f6f0d962d6bf4e8a765
Hanifa Chep <chep.hanifa@gmail.com>
2021-06-22 13:07:18 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* main - computes and prints the sum of all the multioles of 3 or 5 below 1024
+* Return: Always 0
+*/
+
+int main(void)
+{
+int numbers = 0, sum = 0, limit = 1023;
+
+while (numbers <= limit
+{
+if (numbers % 3 == 0 || numbers % 5 == 0)
+			
+sum += numbers;
+
+numbers++;
+
+}
+printf("%i\n", sum);
+
+return (0);
+}
+
