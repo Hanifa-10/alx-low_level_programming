14a574cb8fddac8e315ed0401e7d000c1b5f0b47
Hanifa Chep <chep.hanifa@gmail.com>
2021-06-22 13:32:25 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <limits.h>
+
+/**
+* main - print first 50 fibonacci numbers, starting with 1 and 2
+*
+* Return: 0
+*/
+
+int main(void)
+{
+int count - 50, i;
+long int fNum0 =0, fNum1 = 1;
+long int fSum =0
+
+for (i = 0; i < count; i++)
+{
+fSum = fNum0 +fNum1;
+fNum0 = fNum1;
+fNum1 = fSum;
+
+printf("%li", fSum);
+
+if (i < 49)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return (0);
+}
+
