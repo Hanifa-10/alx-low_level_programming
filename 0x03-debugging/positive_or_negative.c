8b30a94590362ffe5a09905601533b476c91db12
Hanifa Chep <chep.hanifa@gmail.com>
2021-06-23 09:42:19 -0700

    debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -7,18 +7,7 @@
 
 int main(void)
 {
-int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
-
-else
-printf("%d is negative\n", n);
+positive_or_negative(0);
 
 return (0);
 }
