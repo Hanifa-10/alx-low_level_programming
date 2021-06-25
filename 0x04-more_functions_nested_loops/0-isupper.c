afd1b9c8c36742df02a86b595340607bc3a5d857
Hanifa Chep <chep.hanifa@gmail.com>
2021-06-25 01:26:31 -0700

    more functions, nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton"
+
+/**
+* _isupper - checks for uppercase letters
+* @c: character to check
+*
+* Return: 0 or 1
+*/
+
+
+int _isuper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
+
