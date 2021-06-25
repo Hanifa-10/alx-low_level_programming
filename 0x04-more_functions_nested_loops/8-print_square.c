cbaf26c2a4622e5957d75a5000b31be83dd22d31
Hanifa Chep <chep.hanifa@gmail.com>
2021-06-25 12:55:35 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* print_square - print
+* @size: size of the square
+*/
+
+void print_square(int size)
+{
+int row, column;
+
+if (size > 0)
+{
+for (row = 0; row < size; row++)
+{
+for (column = 0; column < size; column++)
+_putchar('#');
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
+
