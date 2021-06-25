f278b4ef2d3e151dd20a509bc5db1c8d2f907c39
Hanifa-10 <84616522+Hanifa-10@users.noreply.github.com>
2021-06-25 23:20:19 +0300

    Update 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of triangle
+ */
+
+void print_triangle(int size)
+{
+	int row, column;
+
+	if (size > 0)
+	{
+		for (row = 1; row <= size; ++row)
+		{
+			for (column = 0; column <= size - 1; ++column)
+			{
+				if (column < (size - row))
+					_putchar(' ');
+				else
+					_putchar('#');
+		}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
+© 2021 GitH
