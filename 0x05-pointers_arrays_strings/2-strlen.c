d0946c239cb5d51e148093a0b7e6c811d093ef02
Hanifa Chep <chep.hanifa@gmail.com>
2021-06-29 19:49:54 -0700

    string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
 * _strlen - return length of a string
 * @s: string
-* Return: always 0
+* Return: 0 or length
 */
 
 int _strlen(char *s)
@@ -13,6 +13,5 @@ int i, length;
 for (i = 0; s[i] != '\0'; i++)
 length++;
 
-return (0);
-}
+return (length);
 
