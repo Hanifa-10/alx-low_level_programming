8a48fe1a5428eebe36eaa7ff66a68e5b81de02b5
Hanifa Chep <chep.hanifa@gmail.com>
2021-07-03 02:19:40 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,14 +9,12 @@
 */
 void puts2(char *str)
 {
-int i = 0;
-
-while (*(str + i))
+int i;
+for (i = 0; str[i] !='\0'; i++)
 {
-_putchar(*(str + i));
-i = i + 2;
+if (i % 2 == 0)
+	_putchar(str[i]);
 }
 _putchar('\n');
-return (void);
 }
 
