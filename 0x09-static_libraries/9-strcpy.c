e56a8ac5e5ad0c4983184e7fd4a5992fe1a933b1
Hanifa-10 <84616522+Hanifa-10@users.noreply.github.com>
2021-07-09 12:10:40 +0300

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,1 +1,23 @@
+#include "holberton.h"
+
+/**
+*_strcpy - copies the string
+* @dest: number
+* @src: number
+* Return: dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+i = 0;
+while (*(src + i) != 0)
+{
+*(dest + i) = *(src + i);
+i++;
+}
+*(dest + i) = *(src + i);
+return (dest);
+}
 
