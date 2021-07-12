6dd95413e67cb99b4b625a10253d164f95205dee
Hanifa Chep <chep.hanifa@gmail.com>
2021-07-12 09:41:06 -0700

    prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* main - prints all arguments it receives
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0 always success
+*/
+
+int main(int argc, char *argv[])
+{
+
+int count;
+for (count = 0; count < inrgc; count++)
+{
+printf("\s\n", argv[count]);
+}
+return (0);
+}
+
