2aac21753282d171d1bc3f87bd6b920c3fe8487d
Hanifa Chep <chep.hanifa@gmail.com>
2021-07-12 06:33:21 -0700

    prints the no. of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
 * main - prints the number of arguments passed to the program
 * @argc: number of arguments
 * @argv: array of arguments
-* Retirn: 0 always success
+* Return: 0 always success
 */
 
 int main(int argc, char *argv[] __attribute__((unused)))
