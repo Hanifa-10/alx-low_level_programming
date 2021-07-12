c0bd45a57c49336560faab9ce913ca5de5cd5ec0
Hanifa Chep <chep.hanifa@gmail.com>
2021-07-12 05:55:01 -0700

    c-arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* main - prints program name
+* @argc: the number of arguments
+* @argv: an array containin the program's arguments
+* Return: 0 always for success
+*/
+
+int main(int argc, char*argv[])
+
+{
+printf("%s\n", argv[argc - 1]);
+
+return (0);
+}
+
---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,6 @@
+#ifndef HOLLBERTON_H
+#define HOLBERTON_H
+
+int main(int argc, char *argv[]);
+#endif
+
