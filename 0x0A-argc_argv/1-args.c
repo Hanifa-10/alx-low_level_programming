8e7f31044f99730a9323fceca9472904110eb9d7
Hanifa Chep <chep.hanifa@gmail.com>
2021-07-12 06:18:26 -0700

    prints the no. of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,9 +9,9 @@
 * Retirn: 0 always success
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-printf("%d\n", arg - 1);
+printf("%d\n", argc - 1);
 
 return (0);
 }
