beb219d759cb74365f46c1d6cc7f779f95ab6c5c
Hanifa-10 <84616522+Hanifa-10@users.noreply.github.com>
2021-07-13 21:39:38 +0300

    Update 101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+* strtow - splits string into words
+* @str: string
+*
+* Return: 0
+*/
+
+char **strtow(char *str)
+{
+
+char *s;
+int len, i;
+
+while (str[len] != '\0')
+len++;
+
+s = malloc(sizeof(char) * (len + 1));
+if (str == NULL || *str == '\0' || s == NULL)
+return (NULL);
+
+
+	for (i = 0; i < len; i++)
+s[i] = str[len];
+
+return (s);
+
+}
