d3702699d40d4770bdc51180b287e387372df7b2
Hanifa Chep <chep.hanifa@gmail.com>
2021-07-13 00:11:06 -0700

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,8 +18,8 @@ for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j]; j++)
 if (argv[i][j] < '0' || argv[i][j] > '9')
-printf("Error\n");
-return (1);
+return (printf("Error\n"), 1);
+
 result += atoi(argv[i]);
 }
 printf("%i\n", result);
