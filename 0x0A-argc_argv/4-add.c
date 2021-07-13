38a4e5c7624b34760091930b3b6a157c4bd11860
Hanifa Chep <chep.hanifa@gmail.com>
2021-07-12 23:55:47 -0700

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds psitive numbers and prints the result
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 always success, 1 for error
+ */
+
+int main(int argc, char *argv[])
+{
+int i, j, result =  0;
+
+if (argc > 1)
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j]; j++)
+if (arg[i][j] < '0' || argv[i][j] > '9')
+printf("Error\n");
+return (1);
+result  += atoi(argv[i];
+}
+printf("%i\n", result);
+return (0);
+}
+
