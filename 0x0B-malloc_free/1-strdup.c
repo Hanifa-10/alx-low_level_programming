94bde12fc5b86320831989f30b920d13abf183cf
Hanifa Chep <chep.hanifa@gmail.com>
2021-07-13 06:16:02 -0700

    c- malloc, free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,20 +13,21 @@
 char *_strdup(char *str)
 {
 char *dup;
-unsigned int i, length;
+int i, length;
 length = 0;
 
 if (str == NULL)
 return (NULL);
-while (str[length])
+for (i = 0; str[i] != '\0'; i++)
 length++;
 
 dup = malloc(sizeof(char) * (length + 1));
 
 if (dup == NULL)
 return (NULL);
-while ((dup[i] = str[i]) != '\0')
-i++;
+for (i = 0; i < length; i++)
+dup[i] = str[i];
+dup[length] = '\0';
 return (dup);
 }
 
