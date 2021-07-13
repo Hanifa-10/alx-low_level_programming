cd996dcdc7c6314cd5842f7a5bdb0cef046c0a3d
Hanifa-10 <84616522+Hanifa-10@users.noreply.github.com>
2021-07-13 21:35:21 +0300

    Update 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+* argstostr - concatenates all arguments
+* @ac: integer
+* @av: char pointer to pointer
+*
+* Return: 0
+*/
+
+char *argstostr(int ac, char **av)
+{
+
+if (ac == 0 || av == NULL)
+return (NULL);
+
+return (*av);
+}
