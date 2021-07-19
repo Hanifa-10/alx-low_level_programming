c3522e071a970c13a480f9798b86461517b8cf05
Hanifa Chep <chep.hanifa@gmail.com>
2021-07-19 11:33:44 -0700

    adding files

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "holberton.h"
+
 /**
 * main - prints the name of the file
 * Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
 printf("%s\n", __FILE__);
 return (0);
