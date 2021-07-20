914a52f9aa39c7c56dda1854a3836059fa6e5179
Hanifa Chep <chep.hanifa@gmail.com>
2021-07-20 05:48:00 -0700

    adding files

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+* init_dog - initialize variable of type struct dog
+* @d: variable to initialize
+* @age: struct element 
+* @owner: struct element
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
+
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+* print_dog -print struct dog
+* @d: pointer of type dog
+*/
+
+void print_dog(struct dog *d)
+{
+if (d != NULL)
+{
+if (d->name != NULL)
+{
+printf("Name: %s\n", d->name);
+}
+else
+printf("%s\n", "Name: (nil)");
+
+if (d->age >= 0)
+{
+printf("Age: %f\n", d->age);
+}
+else
+printf("%s\n", "Age: (nil)");
+
+if (d->owner != NULL);
+{
+printf("Owner: %s\n", d->owner);
+}
+else
+printf("%s\n", "Owner: (nil)");
+}
+}
+
