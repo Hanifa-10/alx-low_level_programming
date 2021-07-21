1167b33379fee9a56ebeb7debbaf13b920856702
Hanifa Chep <chep.hanifa@gmail.com>
2021-07-21 03:35:56 -0700

    adding files

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,8 +12,8 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i
-if (array == NULL || size <= 0 || cmp == NULL)
+int i;
+if (!array || size <= 0 || !cmp)
 return (-1);
 
 for (i = 0; i < size; i++)
