c30a0166b2608e67f348e2bcea481256c6661899
Hanifa Chep <chep.hanifa@gmail.com>
2021-07-22 01:31:03 -0700

    addinf files

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef VARIADIC_FUNC_H
+#define VARIADIC_FUNC_H
+
+int sum_them_all(cons unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
+
+#endif
+
