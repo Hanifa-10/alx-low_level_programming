510a207c6517462515899661c61e24f53322a59f
Hanifa-10 <chep.hanifa@gmail.com>
2021-07-29 16:01:53 -0700

    list length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+#include <stdlib.h>
 /**
 * list_len - returns the number of elements in a linked list_t list
 * @h: pointer to the list to check
@@ -11,12 +11,10 @@ size_t list_len(const list_t *h)
 int c = 0;
 
 while (h)
-
-if (!h)
-return (c);
-else 
+{	
 h = h->next;
 c++;
+}
 return (c);
 }
 
