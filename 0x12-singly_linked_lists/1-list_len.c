d4dbb21dfb900a03a363f66024b351a75eb66a1f
Hanifa-10 <chep.hanifa@gmail.com>
2021-07-29 15:49:50 -0700

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* list_len - returns the number of elements in a linked list_t list
+* @h: pointer to the list to check
+* Return: the numberof elements in a linked list
+*/
+
+size_t list_len(const list_t *h)
+{
+int c = 0;
+
+while (h)
+
+if (!h)
+return (c);
+else 
+h = h->next;
+c++;
+return (c);
+}
+
