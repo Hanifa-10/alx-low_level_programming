599eaa876de02c9d7960736437f217671abd7de4
Hanifa-10 <chep.hanifa@gmail.com>
2021-07-30 16:21:36 -0700

    first function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,6 @@ void first_func(void)__attribute__((constructor));
 
 void first_func(void)
 {
-printf("%s", "You're beat! and yet, you must allow,\n");
-printf("%s", "I bore my back!\n");
+printf("%s", "You're beat! and yet, you must allow,\nI bore my back!\n");
 }
 
