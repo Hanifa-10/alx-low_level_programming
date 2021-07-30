383fe2514e4c4af127759e383f260f12b2b5e6b7
Hanifa-10 <chep.hanifa@gmail.com>
2021-07-30 16:13:51 -0700

    first function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void first_func(void)__attribute__((constructor));
+/**
+* first_func - first function to execute bbefore main
+*/
+
+void first_func(void)
+printf("%s", "You're beat! and yet, you must allow,\n");
+printf("%s", "I bore my back!\n")
+}
+
