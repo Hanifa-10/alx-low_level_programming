88f3f7dac69e1f59ec696b2a753500597985df85
Hanifa-10 <chep.hanifa@gmail.com>
2021-08-03 12:29:35 -0700

    fixing task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,7 +6,7 @@
 * Return: nth node or NULL if node is empty
 */
 
-listint_t *get_node_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int i = 0;
 
