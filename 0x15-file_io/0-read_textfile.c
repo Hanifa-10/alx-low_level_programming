3aadf3e82f3ad71ccd5508df7f7ca039e56f593a
Hanifa-10 <chep.hanifa@gmail.com>
2021-08-10 23:19:06 -0700

    read & print

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,7 @@
 * @letters: the number of letters it should read and print
 * Return: actual number of letters it could read and print, if the file
 * cannot be opened or read, return 0, if filename is NULL return 0, if write
-* fails or does not write the expected amount of byts, return 0
+* fails or does not write the expected amount of bytes, return 0
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
