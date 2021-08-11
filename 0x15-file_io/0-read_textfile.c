40fb8bf26e444d78fd7e9cbfe08b5d165dc2b969
Hanifa-10 <chep.hanifa@gmail.com>
2021-08-11 01:02:01 -0700

    read & print

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,9 +4,7 @@
 * read_textfile - reads a text file and prints it to the POSIX standard output
 * @filename: the name of the file
 * @letters: the number of letters it should read and print
-* Return: actual number of letters it could read and print, if the file
-* cannot be opened or read, return 0, if filename is NULL return 0, if write
-* fails or does not write the expected amount of bytes, return 0
+* Return: actual number of letters it could read and print, 0 on failure
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
