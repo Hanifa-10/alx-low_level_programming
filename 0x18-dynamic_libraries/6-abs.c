1ce14c195bebdc38f8f5a896c0cf058b8ecf43bb
Hanifa-10 <84616522+Hanifa-10@users.noreply.github.com>
2021-09-20 16:03:19 +0300

    Create 6-abs.c

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* _abs -computes absolute value of an integer
+* @n: the number to check
+*
+* Return: absolute value integer
+*/
+
+int _abs(int n)
+{
+if (n < 0)
+n = n * -1;
+return (n);
+}
